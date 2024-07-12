02554395b244700b947a0cbd0fe790b17d133486
ananthvk <123456789987654321sr@gmail.com>
2024-07-13 03:24:12 +0530

    Finished 1671

--- 1671_ShortestRoutesI.cpp
+++ 1671_ShortestRoutesI.cpp
@@ -2,36 +2,31 @@
 #include <queue>
 #include <vector>
 using namespace std;
-const int SRC = 1;
 
 class WeightedDirectedGraph
 {
     struct Edge
     {
-        int vertex;
-        int weight;
+        long long int vertex;
+        long long int weight;
     };
 
     // Adjacency list representation
     vector<vector<Edge>> adj;
 
   public:
-    WeightedDirectedGraph(int sz) : adj(sz) {}
+    WeightedDirectedGraph(long long int sz) : adj(sz) {}
 
     // Adds a weighted directed edge from u->v
-    void add(int u, int v, int w)
+    void add(long long int u, long long int v, long long int w)
     {
-        // Convert from 1 indexed to 0 indexed
-        --u;
-        --v;
         if (u < 0 || u >= adj.size() || v < 0 || v >= adj.size())
             throw std::out_of_range("Invalid vertex number");
         adj[u].push_back({v, w});
     }
 
-    const vector<Edge> &adjacent(int u) const
+    const vector<Edge> &adjacent(long long int u) const
     {
-        --u;
         if (u < 0 || u >= adj.size())
             throw std::out_of_range("Invalid vertex number");
         return adj[u];
@@ -42,16 +37,16 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, m;
+    long long int n, m;
     cin >> n >> m;
 
     WeightedDirectedGraph g(n);
 
-    for (int i = 0; i < m; i++)
+    for (long long int i = 0; i < m; i++)
     {
-        int t1, t2, w;
+        long long int t1, t2, w;
         cin >> t1 >> t2 >> w;
-        g.add(t1, t2, w);
+        g.add(t1 - 1, t2 - 1, w);
     }
 
     // Use Dijkstra's algorithm to solve this problem
@@ -59,15 +54,46 @@ int main()
     // at the cost of a little performance, since useless edges are present in the queue.
 
     // Pair of distance, vertex,
-    priority_queue<pair<int, int>, std::vector<std::pair<int, int>>,
-                   std::greater<std::pair<int, int>>>
+    priority_queue<pair<long long int, long long int>,
+                   std::vector<std::pair<long long int, long long int>>,
+                   std::greater<std::pair<long long int, long long int>>>
         pq;
 
+    long long int source = 0;
+
     // Add the source vertex to priority queue, with distance 0
-    pq.push(make_pair(SRC, 0));
+    pq.push(make_pair(0, source));
+    vector<long long int> dist(n, -1);
+    vector<bool> visited(n, false);
+    dist[source] = 0;
 
     while (!pq.empty())
     {
+        // Choose the vertex with the smallest distance
+        auto v = pq.top();
         pq.pop();
+
+        // This node has already been processed
+        if (visited[v.second])
+            continue;
+
+        // Check all adjacent vertices
+        for (const auto &adj : g.adjacent(v.second))
+        {
+            // If the distance is -1, the vertex is unexplored, i.e. infinite distance, so just set
+            // the distance Check if going through v reduces the distance, if so, perform edge
+            // relaxation
+            if ((dist[adj.vertex] == -1) || (v.first + adj.weight) < dist[adj.vertex])
+            {
+                dist[adj.vertex] = v.first + adj.weight;
+                pq.push(make_pair(dist[adj.vertex], adj.vertex));
+            }
+        }
+        visited[v.second] = true;
     }
+    for (const auto &d : dist)
+    {
+        cout << d << " ";
+    }
+    cout << endl;
 }
